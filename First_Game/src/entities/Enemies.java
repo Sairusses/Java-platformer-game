c06bcd7ef132162597333316a619af3ff6dde433
Sairusses <156464905+Sairusses@users.noreply.github.com>
2024-01-13 04:13:42 +0800

    Add files via upload

--- First_Game/src/entities/Enemies.java
+++ First_Game/src/entities/Enemies.java
@@ -1,7 +1,9 @@
 package entities;
+
 import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 
@@ -9,66 +11,94 @@ public class Enemies {
 
     public List<Mushroom> mushrooms;
     public List<Eye> eyes;
-    
+
     Player player;
+    Random r = new Random();
+    int randomX = r.nextInt(0, 1130);
+    Thread spawnThread;
+
     public Enemies(Player player) {
-    	this.player = player;
+        this.player = player;
         mushrooms = new ArrayList<>();
         eyes = new ArrayList<>();
         spawnEnemies();
+        startSpawnThread();
     }
+
     public void setPlayer(Player player) {
-    	this.player = player;
+        this.player = player;
     }
-    private void spawnEnemies() {
-    	Random r = new Random();
-    	int randomX = r.nextInt(0, 1130);
+
+    public void spawnEnemies() {
+        randomX = r.nextInt(0, 1130);
         spawnMushroom(randomX, 300, player);
         randomX = r.nextInt(0, 1130);
         spawnEye(randomX, 300, player);
+    }
+
+    private void startSpawnThread() {
+        spawnThread = new Thread(() -> {
+            while (true) {
+                try {
+                    Thread.sleep(5000); // Sleep for 5 seconds
+                    randomX = r.nextInt(0, 1130);
+                    spawnMushroom(randomX, 300, player);
+                    randomX = r.nextInt(0, 1130);
+                    spawnEye(randomX, 300, player);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        spawnThread.start();
+    }
+
+    public void stopSpawnThread() {
+        spawnThread.interrupt();
     }
 
     public void spawnMushroom(float x, float y, Player player) {
         Mushroom newMushroom = new Mushroom(x, y, player);
         mushrooms.add(newMushroom);
     }
+
     public void spawnEye(float x, float y, Player player) {
         Eye newEye = new Eye(x, y, player);
         eyes.add(newEye);
     }
-    
+
     public void update() {
         for (Mushroom mushroom : mushrooms) {
-            if(mushroom.isAlive()) mushroom.update();
+            if (mushroom.isAlive()) mushroom.update();
         }
         for (Eye eye : eyes) {
-            if(eye.isAlive()) eye.update();
+            if (eye.isAlive()) eye.update();
         }
     }
-    
+
     public void checkEnemyHit(Rectangle rect) {
-    	for(Mushroom mushroom : mushrooms) {
-    		if(mushroom.isAlive())
-    			if(rect.intersects(mushroom.getHitbox())) {
-        			mushroom.hurt(mushroom.mushroomDamage);
-        			return;
-        		}
-    	}
-    	for(Eye eye : eyes) {
-    		if(eye.isAlive())
-    			if(rect.intersects(eye.getHitbox())) {
-        			eye.hurt(eye.eyeDamage);
-        			return;
-        		}
-    	}
+        for (Mushroom mushroom : mushrooms) {
+            if (mushroom.isAlive())
+                if (rect.intersects(mushroom.getHitbox())) {
+                    mushroom.hurt(mushroom.mushroomDamage);
+                    return;
+                }
+        }
+        for (Eye eye : eyes) {
+            if (eye.isAlive())
+                if (rect.intersects(eye.getHitbox())) {
+                    eye.hurt(eye.eyeDamage);
+                    return;
+                }
+        }
     }
 
     public void render(Graphics g) {
         for (Mushroom mushroom : mushrooms) {
-            if(mushroom.isAlive()) mushroom.render(g);
+            if (mushroom.isAlive()) mushroom.render(g);
         }
         for (Eye eye : eyes) {
-            if(eye.isAlive()) eye.render(g);
+            if (eye.isAlive()) eye.render(g);
         }
     }
 }
